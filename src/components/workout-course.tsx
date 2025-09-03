7c12adacf4125aa50e309f12900f57aa8fe26654
Aa Hh <asmclen@gmail.com>
2025-09-03 15:50:52 +0000

    1. تسجيل الكورس
    المستخدم يدخل عدد أيام التمرين في الأسبوع أو الشهر.
    
    يخ

--- src/components/workout-course.tsx
+++ src/components/workout-course.tsx
@@ -228,7 +228,7 @@ function WorkoutPlanSetup({ config, onSave }: { config: CourseConfig, onSave: (p
                                                             </p>
                                                         </div>
                                                     </div>
-                                                    <Button variant="ghost" size="icon" className="rounded-full h-8 w-8 opacity-0 group-hover:opacity-100" onClick={() => handleRemoveExercise(day, index)}>
+                                                    <Button variant="ghost" size="icon" className="rounded-full h-8 w-8 opacity-0 group-hover:opacity-100" onClick={()={() => handleRemoveExercise(day, index)}>
                                                         <Trash2 className="h-4 w-4 text-destructive" />
                                                     </Button>
                                                 </div>
